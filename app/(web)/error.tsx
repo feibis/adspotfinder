907e54f9e419a96d7c6ffc3cb399617af42b8ff2
Piotr Kulpinski <piotr@kulpinski.pl>
2025-10-02 12:33:00 +0200

    refactor: improve the error page

--- app/(web)/error.tsx
+++ app/(web)/error.tsx
@@ -3,8 +3,8 @@
 import { useEffect } from "react"
 import { Button } from "~/components/common/button"
 import { Link } from "~/components/common/link"
-import { Prose } from "~/components/common/prose"
-import { Intro, IntroTitle } from "~/components/web/ui/intro"
+import { Stack } from "~/components/common/stack"
+import { Intro, IntroDescription, IntroTitle } from "~/components/web/ui/intro"
 import { env } from "~/env"
 
 type ErrorProps = {
@@ -19,19 +19,24 @@ export default function ({ error, reset }: ErrorProps) {
   }, [error])
 
   return (
-    <Intro>
+    <Intro alignment="center">
       <IntroTitle>Something went wrong!</IntroTitle>
 
-      <Prose>
-        <p>
-          Please try again. If the problem persists, contact support at{" "}
-          <Link href={`mailto:${env.NEXT_PUBLIC_SITE_EMAIL}`}>{env.NEXT_PUBLIC_SITE_EMAIL}</Link>.
-        </p>
-      </Prose>
+      <IntroDescription className="max-w-xl">
+        We're sorry, but we encountered an error. Please try again, and if the problem persists,
+        contact support at{" "}
+        <Link href={`mailto:${env.NEXT_PUBLIC_SITE_EMAIL}`}>{env.NEXT_PUBLIC_SITE_EMAIL}</Link>.
+      </IntroDescription>
 
-      <Button className="mt-4" onClick={() => reset()}>
-        Try again
-      </Button>
+      <Stack className="mt-4">
+        <Button variant="fancy" onClick={reset}>
+          Reload the page
+        </Button>
+
+        <Button variant="soft" asChild>
+          <Link href="/">Go back home</Link>
+        </Button>
+      </Stack>
     </Intro>
   )
 }
